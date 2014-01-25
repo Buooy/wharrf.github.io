00c4e5b49ddb953a946b18c6b642378d97e34c65
Buooy <aaron.lee@buooy.com>
2014-01-26 01:56:04 +0800

    First Level Set Up

---
+++ js/script.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($){
+    
+	// MENU
+    $('#menu').multilevelpushmenu({
+    	collapsed: 		true, 
+    	fullCollapse: 	true,
+    	direction: 		'ltr',
+        menuWidth: 		'250px',    
+        wrapperClass: 'multilevelpushmenu_wrapper',
+        containersToPush: [ $( '#menu-button' ) ],
+        onCollapseMenuEnd: function(){
+        	$('#menu-button').data('status','collapse');
+        },
+        onExpandMenuEnd: function(){
+        	$('#menu-button').data('status', 'expand');
+        }
+    });
+
+    $('#menu-button').click(function(){
+    	if( $('#menu-button').data('status') == 'collapse' ){
+    		$('#menu').multilevelpushmenu('expand');
+    	}
+    	else{
+    		$('#menu').multilevelpushmenu('collapse');	
+    	}
+    });
+
+    // MailChimp
+    $('#mailchimp-submit').click(function(){
+    	$('#mc-embedded-subscribe-form').submit();
+    });
+
+});

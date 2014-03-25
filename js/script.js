0e6339a9fc5d5f3dcacb8cc976cd846d0a75b19e
Buooy <aaron.lee@buooy.com>
2014-03-25 16:09:56 +0800

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -59,5 +59,12 @@ jQuery(document).ready(function($){
     $('#mailchimp-submit').click(function(){
     	$('#mc-embedded-subscribe-form').submit();
     });
+  
+  	// Zopim
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?1zrxe6CkX8w8nnISOcvm87mSmVFAzlpJ';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 
 });

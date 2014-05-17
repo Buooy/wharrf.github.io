33c7215d87daccf09f5239cdc4724050523bdbaf
Buooy <aaron.lee@buooy.com>
2014-05-18 02:49:29 +0800

    Updated Wharrf

--- js/script.js
+++ js/script.js
@@ -41,8 +41,10 @@ jQuery(document).ready(function($){
         }, 500);
     });
 
-    // MailChimp
+    // Popover
+    $('.feature').popover();
 
+    // MailChimp
     var attr = location.href;
     attr = attr.split('?');
 
@@ -61,10 +63,11 @@ jQuery(document).ready(function($){
     });
   
   	// Zopim
+    /*
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
 $.src='//v2.zopim.com/?1zrxe6CkX8w8nnISOcvm87mSmVFAzlpJ';z.t=+new Date;$.
 type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-
+*/
 });
